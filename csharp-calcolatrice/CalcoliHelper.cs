8e35872468df8d6a7852f7ee511a71b4e339019d
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-12 16:34:11 +0200

    calculation functions with controls

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -8,6 +8,56 @@ namespace csharp_calcolatrice
 {
     public class CalcoliHelper
     {
+        public static double SommaDueNumeri(double num1, double num2)
+        {
+            return num1 + num2;
+        }
 
+
+        public static double DifferenzaDueNumeri(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double MoltiplicaDueNumeri(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double ValoreAssoluto(double num1)
+        {
+            if (num1 < 0)
+            {
+                return num1 * -1;
+            }
+            else
+            {
+                return num1;
+            }
+        }
+
+        public static double Minimo(double num1, double num2)
+        {
+            if (num1 < num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+
+        public static double Massimo(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
     }
 }
